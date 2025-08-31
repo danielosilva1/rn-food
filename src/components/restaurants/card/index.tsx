b41ba354b624443088774d027ff4c21ac74307a0
vitor <danielosilva@alu.ufc.br>
2025-08-31 16:24:16 -0300

    feat: add the restaurant card component

---
+++ src/components/restaurants/card/index.tsx
@@ -0,0 +1,26 @@
+import { Restaurant } from "@/src/types/restaurant";
+import { Image, Pressable, Text } from "react-native";
+
+interface RestaurantCardProps {
+  restaurant: Restaurant;
+}
+
+export function RestaurantCard({ restaurant }: RestaurantCardProps) {
+  return (
+    <Pressable
+      className="flex flex-col items-center justify-center"
+      onPress={() => console.log(`Clicou no restaurante '${restaurant.name}'`)}
+    >
+      <Image
+        source={{ uri: restaurant.image }}
+        className="w-20 h-20 rounded-full"
+      />
+      <Text
+        className="text-sm mt-2 w-20 text-center leading-4 text-black"
+        numberOfLines={2}
+      >
+        {restaurant.name}
+      </Text>
+    </Pressable>
+  );
+}
